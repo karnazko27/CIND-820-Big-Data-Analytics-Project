e3f3d17f1baf75881e6c388901a620d58ea9abf6
Karnaz O <39681922+karnazko27@users.noreply.github.com>
2024-04-18 13:50:53 +0600

    Delete scripts directory

--- scripts/setup.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-#
-
-# download textblob corpus
-python -m textblob.download_corpora
-# download spacy language model
-python -m spacy download en_core_web_sm
